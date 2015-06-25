8cc059916b5391a4fb8c5a1e06b3d143a1f670f5
Douglas Creager <douglas.creager@redjack.com>
2015-06-25 13:53:31 -0400

    Enable static libraries
    The new ENABLE_SHARED, ENABLE_SHARED_EXECUTABLES, and ENABLE_STATIC
    cmake flags give you full control over whether to build shared and/or
    static libraries, and whether to link any executables against shared
    libraries or static.

--- cmake/FindCTargets.cmake
+++ cmake/FindCTargets.cmake
@@ -8,7 +8,16 @@
 
 
 #-----------------------------------------------------------------------
-# Library
+# Configuration options that control all of the below
+
+set(ENABLE_SHARED YES CACHE BOOL "Whether to build a shared library")
+set(ENABLE_SHARED_EXECUTABLES YES CACHE BOOL
+    "Whether to link executables using shared libraries")
+set(ENABLE_STATIC YES CACHE BOOL "Whether to build a static library")
+
+
+#-----------------------------------------------------------------------
+# Library, with options to build both shared and static versions
 
 function(target_add_shared_libraries TARGET_NAME LIBRARIES LOCAL_LIBRARIES)
     foreach(lib ${LIBRARIES})
@@ -23,6 +32,20 @@ function(target_add_shared_libraries TARGET_NAME LIBRARIES LOCAL_LIBRARIES)
         target_link_libraries(${TARGET_NAME} ${lib}-shared)
     endforeach(lib)
 endfunction(target_add_shared_libraries)
+
+function(target_add_static_libraries TARGET_NAME LIBRARIES LOCAL_LIBRARIES)
+    foreach(lib ${LIBRARIES})
+        string(REPLACE "-" "_" lib ${lib})
+        string(TOUPPER ${lib} upperlib)
+        target_link_libraries(
+            ${TARGET_NAME}
+            ${${upperlib}_STATIC_LIBRARIES}
+        )
+    endforeach(lib)
+    foreach(lib ${LOCAL_LIBRARIES})
+        target_link_libraries(${TARGET_NAME} ${lib}-static)
+    endforeach(lib)
+endfunction(target_add_static_libraries)
 
 set_property(GLOBAL PROPERTY ALL_LOCAL_LIBRARIES "")
 
@@ -46,29 +69,61 @@ function(add_c_library __TARGET_NAME)
     list(APPEND ALL_LOCAL_LIBRARIES ${__TARGET_NAME})
     set_property(GLOBAL PROPERTY ALL_LOCAL_LIBRARIES "${ALL_LOCAL_LIBRARIES}")
 
-    add_library(${__TARGET_NAME}-shared SHARED "${__SOURCES}")
-    set_target_properties(
-        ${__TARGET_NAME}-shared PROPERTIES
-        OUTPUT_NAME ${__OUTPUT_NAME}
-        CLEAN_DIRECT_OUTPUT 1
-        VERSION ${__VERSION}
-        SOVERSION ${__SOVERSION}
-    )
+    if (ENABLE_SHARED OR ENABLE_SHARED_EXECUTABLES)
+        add_library(${__TARGET_NAME}-shared SHARED "${__SOURCES}")
+        set_target_properties(
+            ${__TARGET_NAME}-shared PROPERTIES
+            OUTPUT_NAME ${__OUTPUT_NAME}
+            CLEAN_DIRECT_OUTPUT 1
+            VERSION ${__VERSION}
+            SOVERSION ${__SOVERSION}
+        )
 
-    target_include_directories(
-        ${__TARGET_NAME}-shared PUBLIC
-        ${CMAKE_SOURCE_DIR}/include
-        ${CMAKE_BINARY_DIR}/include
-    )
+        target_include_directories(
+            ${__TARGET_NAME}-shared PUBLIC
+            ${CMAKE_SOURCE_DIR}/include
+            ${CMAKE_BINARY_DIR}/include
+        )
 
-    target_add_shared_libraries(
-        ${__TARGET_NAME}-shared
-        "${__LIBRARIES}"
-        "${__LOCAL_LIBRARIES}"
-    )
+        target_add_shared_libraries(
+            ${__TARGET_NAME}-shared
+            "${__LIBRARIES}"
+            "${__LOCAL_LIBRARIES}"
+        )
 
-    install(TARGETS ${__TARGET_NAME}-shared
-            LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR})
+        # We have to install the shared library if the user asked us to, or if
+        # the user asked us to link our programs with the shared library.
+        install(TARGETS ${__TARGET_NAME}-shared
+                LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR})
+    endif (ENABLE_SHARED OR ENABLE_SHARED_EXECUTABLES)
+
+    if (ENABLE_STATIC OR NOT ENABLE_SHARED_EXECUTABLES)
+        add_library(${__TARGET_NAME}-static STATIC "${__SOURCES}")
+        set_target_properties(
+            ${__TARGET_NAME}-static PROPERTIES
+            OUTPUT_NAME ${__OUTPUT_NAME}
+            CLEAN_DIRECT_OUTPUT 1
+        )
+
+        target_include_directories(
+            ${__TARGET_NAME}-static PUBLIC
+            ${CMAKE_SOURCE_DIR}/include
+            ${CMAKE_BINARY_DIR}/include
+        )
+
+        target_add_static_libraries(
+            ${__TARGET_NAME}-static
+            "${__LIBRARIES}"
+            "${__LOCAL_LIBRARIES}"
+        )
+    endif (ENABLE_STATIC OR NOT ENABLE_SHARED_EXECUTABLES)
+
+    if (ENABLE_STATIC)
+        # We DON'T have to install the static library if the user asked us to
+        # link our programs statically.
+        install(TARGETS ${__TARGET_NAME}-static
+                ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR})
+    endif (ENABLE_STATIC)
 
     set(prefix ${CMAKE_INSTALL_PREFIX})
     configure_file(
@@ -100,11 +155,19 @@ function(add_c_executable __TARGET_NAME)
         ${CMAKE_BINARY_DIR}/include
     )
 
-    target_add_shared_libraries(
-        ${__TARGET_NAME}
-        "${__LIBRARIES}"
-        "${__LOCAL_LIBRARIES}"
-    )
+    if (ENABLE_SHARED_EXECUTABLES)
+        target_add_shared_libraries(
+            ${__TARGET_NAME}
+            "${__LIBRARIES}"
+            "${__LOCAL_LIBRARIES}"
+        )
+    else (ENABLE_SHARED_EXECUTABLES)
+        target_add_static_libraries(
+            ${__TARGET_NAME}
+            "${__LIBRARIES}"
+            "${__LOCAL_LIBRARIES}"
+        )
+    endif (ENABLE_SHARED_EXECUTABLES)
 
     if (NOT __SKIP_INSTALL)
         install(TARGETS ${__TARGET_NAME} RUNTIME DESTINATION bin)
